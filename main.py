737a3f0ee3fe209a27d3a37409aa2134de5c968b
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2018-09-07 19:31:18 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from raven import Client
+
+client = Client('https://8a884d6b0f7d40bcb3bca3fc746cf9b2:94ea70c88887490db7cdc56503368f0c@sentry.io/1276997')
+
+try:
+    1 / 0
+except ZeroDivisionError:
+    client.captureException("brenden")
