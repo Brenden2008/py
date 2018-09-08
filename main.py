7c28f599648e92f52e3a0e02dab2ef680ddd2430
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2018-09-07 19:55:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ client = Client('https://8a884d6b0f7d40bcb3bca3fc746cf9b2:94ea70c88887490db7cdc5
 try:
     1 / 0
 except ZeroDivisionError:
-    client.captureException("brenden")
+    client.captureException()
+end
